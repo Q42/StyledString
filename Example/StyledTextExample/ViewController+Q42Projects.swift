547318fd5041718f867a88e40068871d79e222f0
Tom Lokhorst <tom@lokhorst.eu>
2015-12-12 12:43:18 +0100

    Add Q42 project names to example

---
+++ Example/StyledTextExample/ViewController+Q42Projects.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController+Q42Projects.swift
+//  StyledTextExample
+//
+//  Created by Tom Lokhorst on 2015-12-12.
+//  Copyright © 2015 Tom Lokhorst. All rights reserved.
+//
+
+import Foundation
+import StyledString
+
+extension ViewController {
+  func q42Projects() -> StyledString {
+    return [
+      thin("Swift ") + light("CancellationToken"),
+      thin("Swift ") + light("JsonGen"),
+      light("SegueManager"),
+      light("Promissum"),
+      light("ElementDiff"),
+      light("Styled") + thin("String"),
+      light("Xcode") + thin(".swift"),
+      light("R") + thin(".swift"),
+    ]
+    .joinWithSeparator("\n")
+  }
+
+  private func thin(str: String) -> StyledString {
+    return StyledString(string: str)
+      .withColor(UIColor(red: 0.204, green: 0.204, blue: 0.204, alpha: 1.0))
+      .withFont(UIFont.systemFontOfSize(72, weight: UIFontWeightThin))
+  }
+
+  private func light(str: String) -> StyledString {
+    return StyledString(string: str)
+      .withColor(UIColor.blackColor())
+      .withFont(UIFont.systemFontOfSize(72, weight: UIFontWeightLight))
+  }
+}
